d76b73fe87f5a2b14f49acd30f74ed8750d640a5
Nikita <69730336+forcecore235@users.noreply.github.com>
2020-09-22 23:01:02 +0300

    Changed to 1 function

--- js/main.js
+++ js/main.js
@@ -1,25 +1,12 @@
-let button1 = document.querySelector(".buttonChange1");
-let button3 = document.querySelector(".buttonChange3");
-let button7 = document.querySelector(".buttonChange7");
 let progressBar = document.querySelector(".progress-bar");
+let btn = document.querySelectorAll(".btn");
 
-button1.addEventListener("click", function() {
-    let progressValue = progressBar.style.width.slice(0, -1);
-    let newValue = Number(progressValue) + 1;
-    console.log(newValue);
-    progressBar.style.width = [newValue + "%"];
-})
+let progressValue = progressBar.style.width.slice(0, -1);
 
-button3.addEventListener("click", function() {
-    let progressValue = progressBar.style.width.slice(0, -1);
-    let newValue = Number(progressValue) + 3;
-    console.log(newValue);
-    progressBar.style.width = [newValue + "%"];
-})
-
-button7.addEventListener("click", function() {
-    let progressValue = progressBar.style.width.slice(0, -1);
-    let newValue = Number(progressValue) + 7;
-    console.log(newValue);
-    progressBar.style.width = [newValue + "%"];
+btn.forEach((element) => {
+    element.addEventListener("click", function() {
+        progressValue = Number(progressValue) + Number(element.value);
+        console.log(progressValue);
+        progressBar.style.width = [progressValue + "%"];
+    });
 })
